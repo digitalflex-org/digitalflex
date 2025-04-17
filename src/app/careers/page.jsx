075b0771bd44df3ef7c8cc70568e4c76d9d229c1
yusuf <horiyorrmi72@gmail.com>
2025-04-17 23:31:35 +0100

    chores(modify design)modify design with mock data or dummy api

--- src/app/careers/page.jsx
+++ src/app/careers/page.jsx
@@ -1,6 +1,6 @@
-import WhyWorkWithUs from "../../components/careers/whyWorkWitUs"
-import CareersHero from "../../components/careers/careerHero"
-import OpenPositions from "../../components/careers/openPositions"
+import {WhyWorkWithUs} from "../../components/careers/whyWorkWitUs"
+import {CareersHero} from "../../components/careers/careerHero"
+import {OpenPositions} from "../../components/careers/openPositions"
 
 
 const CareersPage = () => {
@@ -9,7 +9,7 @@ const CareersPage = () => {
       <CareersHero />
       <WhyWorkWithUs />
       <div>
-        <OpenPositions />
+        <OpenPositions/>
       </div>
     </div>
   )
