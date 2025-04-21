ad041fcdedc86f0dcb3216328f952a1f245f411e
yusuf <horiyorrmi72@gmail.com>
2025-04-21 23:42:20 +0100

    fix(overflow of description):fix overflow of job description

--- src/app/careers/[slug]/page.jsx
+++ src/app/careers/[slug]/page.jsx
@@ -8,7 +8,7 @@ export default async function Page({ params }) {
   if (!job) return <div>Job not found</div>;
 
   return (
-    <div className="w-full p-4 md:p-6 flex flex-col md:flex-row gap-6 md:h-screen bg-gray-50 text-black">
+    <div className="w-full p-4 md:p-6 flex flex-col md:flex-row gap-6 md:h-auto bg-gray-50 text-black">
       {/* Job Details Section */}
       <div className="w-full md:w-2/3">
         <h1 className="text-2xl md:text-3xl font-bold mb-4">{job.title}</h1>
