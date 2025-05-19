af079bfa67439d86b6dccf0b7a1014240007f310
yusuf <horiyorrmi72@gmail.com>
2025-05-19 16:23:59 +0100

    feat(axios) use axios for requests

---
+++ src/lib/axios.tsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const api = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+    timeout: 10000,
+});
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        console.error("API Error:", error.message);
+        return Promise.reject(error);
+    }
+);
