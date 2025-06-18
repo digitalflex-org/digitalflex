425ed37c1bd61e44699025f3af7ba44f46c86f3b
yusuf <horiyorrmi72@gmail.com>
2025-06-18 20:45:30 +0100

    feat(auth):add single auth page

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -2,8 +2,9 @@
 
 import { useState, useEffect, Suspense } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
-import Signin from 'components/auth/signin';
-import Signup from 'components/auth/signup';
+import Signin from '@/components/auth/signin';
+import Signup from '@/components/auth/signup';
+import Spinner from '@/components/spinner';
 
 // Child component using useSearchParams
 const AuthPageContent = () => {
@@ -11,6 +12,7 @@ const AuthPageContent = () => {
   const router = useRouter();
 
   const [showSignIn, setShowSignIn] = useState(false);
+  const callbackUrl = searchParams.get('callbackUrl')
 
   useEffect(() => {
     const tab = searchParams.get('tab');
@@ -18,7 +20,8 @@ const AuthPageContent = () => {
   }, [searchParams]);
 
   const handleTabChange = (tab) => {
-    router.push(`?tab=${tab}`);
+    const cb = searchParams.get('callbackUrl') || '';
+    router.push(`?tab=${tab}&callbackUrl=${cb}`);
   };
 
   return (
@@ -54,7 +57,7 @@ const AuthPageContent = () => {
 const Page = () => {
   return (
     <div className="min-h-screen bg-gray-100 text-black py-12 px-4">
-      <Suspense fallback={<div className="text-center text-gray-500">Loading...</div>}>
+      <Suspense fallback={<Spinner />}>
         <AuthPageContent />
       </Suspense>
     </div>
