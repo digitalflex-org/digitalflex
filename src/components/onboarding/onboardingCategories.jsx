0359c3f451093a3aaa025e63f082379e3de8b83d
yusuf <horiyorrmi72@gmail.com>
2025-06-18 21:00:29 +0100

    chores(proceed):change CTA to proceed

--- src/components/onboarding/onboardingCategories.jsx
+++ src/components/onboarding/onboardingCategories.jsx
@@ -1,38 +1,39 @@
 'use client';
 import React from 'react'
-// import { useSearchParams } from 'next/navigation';
 import { api } from '../../lib/axios';
 import Link from 'next/link';
 
-const onboardingCategories = () => {
-    const title = 'Materials'
-    const onboardingSections = [{
-        "title": "Logic",
-        "description": "This section will provide you with materials to test your logic accuraccy.",
-        "category":"logic"
-        
-    },{
-        "title": "Tech Readiness",
-        "description": "This section will test your computer literacy",
-        "category":"tech-readiness"
-        
-        },
-        {
-            "title": "mindset",
-            "description": `This section will provide you with materials to test your mindset.`,
-            "category":"mindset"
-            
-        },
-        {
-            "title": "Others",
-            "description": `As the title of the section implies, you will be provided with random materials that will enhance your management skills.`,
-            "category":"others"
-            
-        }
-    ]
 
-    return (
-    <section className="bg-white py-12 px-8">
+const onboardingCategories = () => {
+
+  const title = 'Resource Hub'
+  const onboardingSections = [{
+    "title": "Logic",
+    "description": "This section will provide you with materials to test your logic accuraccy.",
+    "category": "logic"
+
+  }, {
+    "title": "Tech Readiness",
+    "description": "This section will test your computer literacy",
+    "category": "tech-readiness"
+
+  },
+  {
+    "title": "mindset",
+    "description": `This section will provide you with materials to test your mindset.`,
+    "category": "mindset"
+
+  },
+  {
+    "title": "Others",
+    "description": `As the title of the section implies, you will be provided with random materials that will enhance your management skills.`,
+    "category": "others"
+
+  }
+  ]
+
+  return (
+    <section className="bg-white py-12 px-8 font-serif">
       <div className="container mx-auto">
         <h2 className="text-3xl font-bold mb-6 text-blue-900">{title}</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
@@ -41,12 +42,12 @@ const onboardingCategories = () => {
               <h3 className="text-xl font-semibold mb-4 capitalize">{boardingSection.title}</h3>
               <p className="mb-4">{boardingSection.description}</p>
               <Link href={`/onboarding/materials?category=${boardingSection.category}`}><button className="bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 cursor-pointer">
-                Complete Material
+                Proceed...
               </button></Link>
             </div>
           ))}
-          </div>
-          <p className='text-black mt-5 text-xl'><span className='font-semibold mr-4'>NB:</span>Upon completing each section click the complete button to show you've completed the section. </p>
+        </div>
+        <p className='text-black mt-5 text-xl'><span className='font-semibold mr-4'>NB:</span>Upon completing each section click the complete button to show you've completed the section. </p>
       </div>
     </section>
   )
