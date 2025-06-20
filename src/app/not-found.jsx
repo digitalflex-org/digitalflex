644b6234aaf2e8319bc11ec3e94577825d6eeab9
yusuf <horiyorrmi72@gmail.com>
2025-06-20 22:28:54 +0100

    chores(fix & style):fix motion import issue and style page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,16 +1,21 @@
 import Link from 'next/link';
-import { motion } from 'framer-motion';
 import { ArrowLeft } from 'lucide-react';
 
 export default function NotFound() {
     return (
-        <div className='flex flex-col items-center h-[350px] m-auto'>
-            <h2 className='text-2xl'>Not Found</h2>
-            <p>Could not find requested resource</p>
+        <div className="flex flex-col items-center justify-center min-h-[350px] max-w-md mx-auto text-center p-6">
+            <h2 className="text-4xl font-semibold text-gray-800 mb-2">Page Not Found</h2>
+            <p className="text-gray-600 mb-6">
+                Sorry, we couldn’t find the page you’re looking for.
+            </p>
 
-            <Link className='text-[#1d3c6a] flex gap-2' href='/'> <ArrowLeft className='text-[#1d3c6a]' /> Return Home</Link>
-
-
+            <Link
+                href="/"
+                className="inline-flex items-center gap-2 px-4 py-2 rounded-md text-white bg-[#004e89] hover:bg-blue-700 transition-colors duration-200"
+            >
+                <ArrowLeft className="w-5 h-5" />
+                Return Home
+            </Link>
         </div>
     );
 }
